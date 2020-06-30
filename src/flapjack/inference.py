70ea1cf5363dbb7055cf8172d297b40311888274
jandremarais <maraisjandre9@gmail.com>
2020-06-30 04:03:08 +0200

    utils

--- src/flapjack/inference.py
+++ src/flapjack/inference.py
@@ -1,6 +1,13 @@
-import pytorch_lightning as pl
-from flapjack.model import Model
+from pathlib import Path
 
+import numpy as np
+import pytorch_lightning as pl
+import torch
+from matplotlib import pyplot as plt
+from torchvision import transforms
+
+from flapjack.model import Model
+from flapjack.utils import warp_from_target, plot
 
 model = Model.load_from_checkpoint(
     "lightning_logs/version_52/checkpoints/epoch=30.ckpt"
@@ -8,3 +15,56 @@ model = Model.load_from_checkpoint(
 
 model.eval()
 
+model.valid_ds.image_fns
+
+# predict on pairs
+# predict on tile
+# predict on ortho
+
+
+
+data = Path('/home/jan/data/aero/')
+
+rgbp = data / 'crops/valid/images/image_8494_1593477617381.npy'
+redp = data / 'crops/valid/targets/target_8494_1593477617381.npy'
+
+im_all = np.load(rgbp)
+rgb = im_all[..., :3]
+red = im_all[..., 3]
+
+tfms = transforms.Compose([
+
+])
+
+
+
+
+
+def predict_pair(rgb: np.ndarray, red: np.ndarray, model):
+    """[summary]
+
+    Args:
+        rgb (np.ndarray): Image array or RGB tile with values in (0,255).
+        red (np.ndarray): Image array of narrowbind tile with shape (H, W).
+    """
+    h, w = red.shape
+    assert rgb.shape[:2] == (h, w)
+    assert h == w
+    x = np.concatenate([rgb.astype(np.float32), red[..., None]], -1)
+    tx = model.valid_ds.transform(x)
+    yhat = model(tx[None].to(model.device))
+    yhat = yhat.view(4, 2).detach().cpu().numpy()
+    return warp_from_target(x, yhat)
+
+
+px, mat = predict_pair(rgb, red, model)
+
+mat
+
+plot(px, figsize=(10, 10))
+plot(px, figsize=(10, 10), sidebyside=False)
+
+
+torch.flatten(torch.tensor([[1, 2], [3, 4]]))
+
+rgb.max()
--- src/flapjack/utils.py
+++ src/flapjack/utils.py
@@ -1,6 +1,8 @@
-from torch.utils.data import DataLoader, Dataset
-import numpy as np
+from typing import Tuple
+
 import cv2
+import numpy as np
+from torch.utils.data import DataLoader, Dataset
 
 
 def compute_stats(ds: Dataset):
@@ -27,11 +29,23 @@ def compute_stats(ds: Dataset):
     return mean, std
 
 
-def warp_from_target(x: np.ndarray, y: np.ndarray) -> np.ndarray:
+def warp_from_target(x: np.ndarray, y: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
     sz = x.shape[0]
     dst = np.array([[0, 0], [sz - 1, 0], [sz - 1, sz - 1], [0, sz - 1]]).astype(
         np.float32
     )
     warp_mat = cv2.getAffineTransform(dst[:3] - y[:3], dst[:3])
     red_aligned = cv2.warpAffine(x[..., -1], warp_mat, (sz, sz))
-    return np.concatenate([x[..., :3], red_aligned[..., None]], -1)
+    return np.concatenate([x[..., :3], red_aligned[..., None]], -1), warp_mat
+
+
+def plot(x, y=None, sidebyside=True, **kwargs):
+    if sidebyside:
+        fig, ax = plt.subplots(1, 2, **kwargs)
+        ax[0].imshow(x[..., :3].astype(np.uint8))
+        ax[1].imshow(x[..., 3], cmap="gray")
+    else:
+        fig, ax = plt.subplots(1, 1, **kwargs)
+        ax.imshow(x[..., :3].astype(np.uint8), alpha=0.5)
+        ax.imshow(x[..., 3], cmap="gray", alpha=0.5)
+    return ax
